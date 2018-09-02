3869992e9990bfafea52662723cc696ea6ff95f6
Sergey Kovalskiy <sergey@kovalskiy.net>
2018-09-02 13:06:36 +0300

    Use postgres for tests (again)

--- schedules/schedules/settings.example.py
+++ schedules/schedules/settings.example.py
@@ -1,10 +1,3 @@
 from schedules.base_settings import *  # NOQA
 
 SECRET_KEY = ''
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'db.sqlite3',
-#     }
-# }
