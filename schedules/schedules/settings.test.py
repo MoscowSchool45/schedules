3ec9ec04323126f9b9317f31635068ad49f097f0
Sergey Kovalskiy <sergey@kovalskiy.net>
2018-09-02 12:54:29 +0300

    Use sqlite for testing

--- schedules/schedules/settings.test.py
+++ schedules/schedules/settings.test.py
@@ -3,3 +3,10 @@ import os
 from schedules.base_settings import *  # NOQA
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
